5e0f59042bbd5e99d351719c36762634a36a2c77
Will Dolezal <willdolezal@gmail.com>
2023-04-22 19:27:07 -0700

    Initialize
    First

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+
+DATE_COLUMN = 'date'
+DATA_URL = 'data.csv'
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    return data
+
+
+data = load_data(1000)
+ycols = data.columns.values.tolist()
+
+fig = px.box(data,x='date',y=ycols[1:])
+
+
+st.title('Ranking Fluctuations for All Sites')
+st.plotly_chart(fig, theme='steamlit', use_container_width=True)
